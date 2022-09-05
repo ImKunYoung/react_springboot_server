16ee6c572f5822e2ce05ebd041536c9599f981f0
ImKunYoung <lim8667@gmail.com>
2022-09-05 19:21:23 +0900

    init commit

---
+++ src/main/java/com/example/react_springboot_server/ReactSpringbootServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_springboot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringbootServerApplication.class, args);
+    }
+
+}
